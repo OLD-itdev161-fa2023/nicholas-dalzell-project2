9131a5075fd82fc86adb04dbf644ae4d4b35240b
Nick Dalzell <dalzelln@gmatc.matc.edu>
2022-12-01 00:43:33 -0600

    working...

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import User from './models/User';
 
 //init express app
 const app = express();
@@ -31,7 +35,7 @@ app.get('/', (req, res) =>
  * @desc Register user
  */
 app.post(
-    '/api/users', 
+    '/api/user', 
     [
         check('name', 'Please enter your name')
             .not()
@@ -42,12 +46,53 @@ app.post(
             'Please enter a password with 6 or more characters'
         ).isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         } else {
-            return res.send(req.body);
+            const { name, email, password} = req.body;
+            try {
+                //check if user exists
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'User already exists' }] });
+                }
+
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                //encrypt password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                //save to db and return
+                await user.save();
+
+                //generate and return jwt token
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr'},
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({ token: token });
+                    }
+                );
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
         }
     }
 );
