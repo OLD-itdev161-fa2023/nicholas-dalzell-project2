ff36202cf4e7c89c228ef7fa3bef04db1dc0f17e
Nick Dalzell <dalzelln@gmatc.matc.edu>
2022-11-30 23:03:02 -0600

    working...

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000')
+    .then((response) => {
+      this.setState({
+        data: response.data
+      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          Project 2
+        </header>
+        {this.state.data}
+      </div>
+    );
+  }
+}
+
+export default App;
